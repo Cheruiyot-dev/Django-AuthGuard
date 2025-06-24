ec9f897f8560a45552984e15d7283a908dd4119f
Cheruiyot-dev <allankipkoech9@gmail.com>
2025-06-24 05:24:13 +0300

    feat: implement custom User model with security fields

--- django_auth_system/accounts/models.py
+++ django_auth_system/accounts/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
+from django.utils import timezone
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=15, blank=True)
+    is_verified = models.BooleanField(default=False)
+    verification_token = models.UUIDField(default=uuid.uuid4, editable=False)
+    failed_login_attempts = models.IntegerField(default=0)
+    account_locked_until = models.DateTimeField(null=True, blank=True)
+    last_login_ip = models.GenericIPAddressField(null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def is_account_locked(self):
+        if self.account_locked_until and self.account_locked_until > timezone.now():
+            return True
+        return False
+
+    def reset_failed_attempts(self):
+        self.failed_login_attempts = 0
+        self.save()
+
